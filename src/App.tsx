7ad92599878296ca094d7bac06793e9f3420aaf2
ilya228081337-ship-it <ilya228081337@gmail.com>
2026-08-12 18:54:45 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -434,7 +434,7 @@ function WhyUs() {
 
 /* ─── About NOW Foods ─── */
 const nowPhotos = [
-  'https://images.pexels.com/photos/7722648/pexels-photo-7722648.jpeg?auto=compress&cs=tinysrgb&w=800',
+  'https://s3.twcstorage.ru/7aa0da8f-f28e-418d-8a6d-fdaec848631b/25.08.22.webp',
   'https://s3.twcstorage.ru/7aa0da8f-f28e-418d-8a6d-fdaec848631b/tcdvambbc2cd099z9h0xps6ddt5ilmr8.webp',
   'https://s3.twcstorage.ru/7aa0da8f-f28e-418d-8a6d-fdaec848631b/p1h90vmfcg125b2nvj9amamuk35.webp',
 ];
